e12e6c7124097518f735a170398bda70496be7cd
Davide Mariani <davidemariani.ai@gmail.com>
2021-12-05 12:03:33 +0000

    main theme changed to 'lanyon'

--- conf.py
+++ conf.py
@@ -154,7 +154,7 @@ NAVIGATION_ALT_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "bootblog4"
+THEME = "lanyon"
 
 # A theme color. In default themes, it might be displayed by some browsers as
 # the browser UI color (eg. Chrome on Android). Other themes might also use it
