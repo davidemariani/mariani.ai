d2421d6b44421497192e1d229a74d34dcceadaab
Davide Mariani <davidemariani.ai@gmail.com>
2021-12-06 19:31:53 +0000

    comments added to posts - disqus integration

--- conf.py
+++ conf.py
@@ -1022,7 +1022,7 @@ COMMENT_SYSTEM = "disqus"
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = ""
+COMMENT_SYSTEM_ID = "mariani-ai"
 
 # Create index.html for page folders?
 # WARNING: if a page would conflict with the index file (usually
