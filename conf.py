ee83d9ee8b27d9a30bdd3c2cceb9984702e85c5e
Davide Mariani <davidemariani.ai@gmail.com>
2021-12-07 23:49:13 +0000

    general look and feel is now ok - new attempt with ipynb using imported images

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "Davide Mariani"  # (translatable)
-BLOG_TITLE = "Davide Mariani's Homepage"  # (translatable)
+BLOG_TITLE = "Davide Mariani's personal blog"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://mariani.ai/"
