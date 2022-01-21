eac6756f547905bdb4785c15f424725e692619c8
Davide Mariani <davidemariani.ai@gmail.com>
2022-01-21 11:44:37 +0100

    google analytics tracking script added in conf.py

--- conf.py
+++ conf.py
@@ -1273,7 +1273,15 @@ MARKDOWN_EXTENSIONS = ['markdown.extensions.fenced_code', 'markdown.extensions.c
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # (translatable)
-# BODY_END = ""
+BODY_END = """  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-QR3BXMGB1N"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-QR3BXMGB1N');
+  </script>"""
 
 # The possibility to extract metadata from the filename by using a
 # regular expression.
